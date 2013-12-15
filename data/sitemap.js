6f0f0e4ae68782a812c59d0ead8e0c4786f5e952
jnuleavin <1786310234@qq.com>
2013-12-15 17:30:31 +0800

    资源

---
+++ data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s),_(c,t,e,f,g,u),_(c,v,e,f,g,w),_(c,x,e,f,g,y),_(c,z,e,f,g,A),_(c,B,e,f,g,C),_(c,D,e,f,g,E),_(c,F,e,f,g,G),_(c,H,e,f,g,I)])]);}; 
+var b="rootNodes",c="pageName",d="主页",e="type",f="Wireframe",g="url",h="主页.html",i="children",j="regist",k="regist.html",l="login",m="login.html",n="方便面",o="方便面.html",p="饼干",q="饼干.html",r="苹果",s="苹果.html",t="购物车",u="购物车.html",v="超市",w="超市.html",x="书店",y="书店.html",z="话费充值",A="话费充值.html",B="生成订单",C="生成订单.html",D="订单详情",E="订单详情.html",F="管理员登录",G="管理员登录.html",H="管理员",I="管理员.html";
+return _creator();
+})();
