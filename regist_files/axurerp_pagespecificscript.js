ae3a4db34819b16ca40bd6c21a0458b697a505e0
jnuleavin <1786310234@qq.com>
2013-12-15 17:37:10 +0800

    登陆与注册

---
+++ regist_files/axurerp_pagespecificscript.js
@@ -0,0 +1,17 @@
+﻿for(var i = 0; i < 12; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u4'] = 'center';gv_vAlignTable['u5'] = 'top';gv_vAlignTable['u7'] = 'top';gv_vAlignTable['u8'] = 'top';
+u11.style.cursor = 'pointer';
+$axure.eventManager.click('u11', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('login.html');
+
+}
+});
+gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u2'] = 'top';
