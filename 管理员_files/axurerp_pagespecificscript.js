f8ffac84e430711f2dcb7d65c97be5a8893089db
sitkit <408789469@qq.com>
2013-12-16 09:10:19 +0800

    项目
    话费 管理员

---
+++ 管理员_files/axurerp_pagespecificscript.js
@@ -0,0 +1,18 @@
+﻿for(var i = 0; i < 57; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u36'] = 'top';gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u17'] = 'top';u8.tabIndex = 0;
+
+u8.style.cursor = 'pointer';
+$axure.eventManager.click('u8', function(e) {
+
+if (true) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('管理员.html'), "");
+
+}
+});
+gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u30'] = 'top';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u11'] = 'top';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u39'] = 'top';gv_vAlignTable['u27'] = 'top';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u42'] = 'top';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u25'] = 'center';gv_vAlignTable['u46'] = 'center';gv_vAlignTable['u56'] = 'top';gv_vAlignTable['u54'] = 'center';gv_vAlignTable['u19'] = 'center';gv_vAlignTable['u55'] = 'top';gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u48'] = 'top';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u47'] = 'top';gv_vAlignTable['u50'] = 'center';gv_vAlignTable['u51'] = 'top';gv_vAlignTable['u52'] = 'top';gv_vAlignTable['u33'] = 'top';
---
+++ 话费充值_files/axurerp_pagespecificscript.js
@@ -0,0 +1,85 @@
+﻿for(var i = 0; i < 41; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u31'] = 'top';gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u28'] = 'top';gv_vAlignTable['u29'] = 'top';gv_vAlignTable['u21'] = 'top';u6.tabIndex = 0;
+
+u6.style.cursor = 'pointer';
+$axure.eventManager.click('u6', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('login.html');
+
+}
+});
+gv_vAlignTable['u6'] = 'top';document.getElementById('u15_img').tabIndex = 0;
+
+u15.style.cursor = 'pointer';
+$axure.eventManager.click('u15', function(e) {
+
+if (true) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('购物车.html'), "");
+
+}
+});
+u13.tabIndex = 0;
+
+u13.style.cursor = 'pointer';
+$axure.eventManager.click('u13', function(e) {
+
+if (true) {
+
+	self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+
+}
+});
+gv_vAlignTable['u13'] = 'top';
+u14.style.cursor = 'pointer';
+$axure.eventManager.click('u14', function(e) {
+
+if ((GetWidgetText('u10')) == ('方便面')) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('话费充值.html'), "");
+
+}
+else
+if ((GetWidgetText('u10')) == ('苹果')) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('苹果.html'), "");
+
+}
+else
+if ((GetWidgetText('u10')) == ('饼干')) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('饼干.html'), "");
+
+}
+});
+gv_vAlignTable['u40'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u37'] = 'top';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u12'] = 'center';document.getElementById('u39_img').tabIndex = 0;
+
+u39.style.cursor = 'pointer';
+$axure.eventManager.click('u39', function(e) {
+
+if (true) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('订单详情.html'), "");
+
+}
+});
+gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u35'] = 'top';u7.tabIndex = 0;
+
+u7.style.cursor = 'pointer';
+$axure.eventManager.click('u7', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('regist.html');
+
+}
+});
+gv_vAlignTable['u7'] = 'top';gv_vAlignTable['u24'] = 'top';gv_vAlignTable['u18'] = 'center';gv_vAlignTable['u19'] = 'top';gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u22'] = 'top';gv_vAlignTable['u33'] = 'top';
